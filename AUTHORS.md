81728af6b93633b9971296dd5f83314959f284b3
Rebecca Hanlon <ryhanlon@gmail.com>
2017-06-16 11:39:42 -0700

    Made/added: static files, base.html, migration and various files.

---
+++ AUTHORS.md
@@ -0,0 +1,1 @@
+by Rebecca York Hanlon
--- Storia/Storia/settings.py
+++ Storia/Storia/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'library.apps.LibraryConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
