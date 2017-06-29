09589bedafb129d533f3dc56b5cfd6a3996320cb
Rebecca Hanlon <ryhanlon@gmail.com>
2017-06-29 12:28:33 -0700

    Audio media working, word per click sounding.  Still issue with autoplay when page loads.

--- Storia/book/static/book/js/script.js
+++ Storia/book/static/book/js/script.js
@@ -1,5 +1,27 @@
-/*
- Created on.
-*/
+// js for story_page.html
+
+function play_sound() {
+    // "use strict";
 
 
+    // audio for word click
+    this.play();
+}
+
+
+
+// $('.play_sound').on('click', function() {
+//     "use strict";
+//
+//       this.play();
+//
+//   }
+// );
+
+//
+// $('#audio').on('click', function () {
+//
+//         $(this).play();
+//
+//     }
+// );
