0d3a229184a57398c8c32aa09800ce0be16e0c7e
Rebecca Hanlon <ryhanlon@gmail.com>
2017-07-07 17:30:16 -0700

    Added mew model to admin.py

--- Storia/accounts/admin.py
+++ Storia/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, Profile
 from insights.admin import AwardanceInline
 from insights.models import AssetInteraction, TimeInteraction
 
@@ -22,3 +22,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
